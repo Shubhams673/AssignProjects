6db0ae9f66be24614b2ca6633ad3589e8952cabc
Subham Kumar Singh <shubhams673@gmail.com>
2025-02-23 23:08:28 +0530

    Update Assignment2.java

--- src/com/assignment/projects/Assignment2.java
+++ src/com/assignment/projects/Assignment2.java
@@ -13,31 +13,31 @@ public class Assignment2 {
 		o Print Semester 3 - Subject 4 and Subject 5 Names.
 		o Print Semester 5 - Subject 3 and Subject 6 marks
 			
-	Semester 	Subject1 				Subject2 			Subject3 			Subject4 				Subject5 				Subject6
-	Sem 1 		Mathematics-I 			Physics 			Chemistry 			Computer Programming	Engineering Drawing		Basic Electrical Eng.
-	Marks 		78 						85 					91 					74 						88						79
+	Semester 	Subject1 			Subject2 			Subject3 			Subject4 				Subject5 			Subject6
+	Sem 1 		Mathematics-I 			Physics 			Chemistry 			Computer Programming			Engineering Drawing		Basic Electrical Eng.
+	Marks 		78 				85 				91 				74 					88				79
 	
-	Semester 	Subject1 				Subject2 			Subject3 			Subject4 				Subject5 				Subject6
-	Sem 2 		Mathematics-II 			Mechanics 			Environmental Sci. 	Basic Electronics Engg. Physics 				Engineering Graphics
-	Marks 		82 						77 					93 					69 						84 						90
+	Semester 	Subject1 			Subject2 			Subject3 			Subject4 				Subject5 			Subject6
+	Sem 2 		Mathematics-II 			Mechanics 			Environmental Sci. 		Basic Electronics Engg. 		Physics 			Engineering Graphics
+	Marks 		82 				77 				93 				69 					84 				90
 	
-	Semester 	Subject1 				Subject2 			Subject3 			Subject4 				Subject5 				Subject6
-	Sem 3 		DataStructuresDiscrete 	Mathematics			Digital				ElectronicsOptngSystems Signals and Systems 	Object-Oriented Prog.
-	Marks 		88 						81 					76 					92 						85 						78
+	Semester 	Subject1 			Subject2 			Subject3 			Subject4 				Subject5 			Subject6
+	Sem 3 		DataStructuresDiscrete 		Mathematics			Digital				ElectronicsOptngSystems 		Signals and Systems 		Object-Oriented Prog.
+	Marks 		88				81 				76 				92 					85 				78
 		
-	Semester 	Subject1 				Subject2 			Subject3 			Subject4 				Subject5 				Subject6
-	Sem 4 		Algorithms Computer		Networks			Database			SystemsMicroprocessors 	Communication Eng. 		Software Engineering
-	Marks 		91 						73 					89 					80 						76 						87
+	Semester 	Subject1 			Subject2 			Subject3 			Subject4 				Subject5 			Subject6
+	Sem 4 		Algorithms Computer		Networks			Database			SystemsMicroprocessors 			Communication Eng. 		Software Engineering
+	Marks 		91 				73 				89 				80 					76 				87
 		
-	Semester 	Subject1 				Subject2 			Subject3 			Subject4 				Subject5 				Subject6
-	Sem 5 		Probability & Stats. 	Machine Learning 	Compiler Design 	Theory of Computation 	Embedded Systems 		Computer Graphics
-	Marks 		86 						88 					84 					95 						73 						90
+	Semester 	Subject1 			Subject2 			Subject3 			Subject4 				Subject5 			Subject6
+	Sem 5 		Probability & Stats. 		Machine Learning 		Compiler Design 		Theory of Computation 			Embedded Systems 		Computer Graphics
+	Marks 		86 				88 				84 				95 					73 				90
 		
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		String [][][] gradStats = { 
+		String [][][] gradStats = {
 									{ 
 										{"Semester", "Sem1", "Marks"}, {"Subject1", "Mathematics-I", "78"}, {"Subject2", "Physics", "85"}, {"Subject3", "Chemistry", "91"}, {"Subject4", "Computer Programming", "74"}, {"Subject5", "Engineering Drawing", "88"}, {"Subject6", "Basic Electrical Eng.", "79"}
 										
