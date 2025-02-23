9ff870c1f744fc7580b0ec6f959c6449bb3cfc50
shubhams673 <shubhams673@gmail.com>
2025-02-23 22:05:57 +0530

    second commit

--- src/com/assignment/projects/Assignment1.java
+++ src/com/assignment/projects/Assignment1.java
@@ -2,9 +2,120 @@ package com.assignment.projects;
 
 public class Assignment1 {
 
+	/**
+	 * 
+	 * @param args
+	 * To Create the Data Types for all the listed values below & Print them.
+	 * 	1. Temperature of a city in degrees Celsius: 25.5
+		2. Whether a customer has placed an order: true or false
+		3. Person's phone number: "123-456-7890"
+		4. Amount of money in a customer's bank account: 1000.50
+		5. Person's email address: "john.doe@example.com"
+		6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		7. Person's marital status: true or false
+		8. Person's occupation: "Software Engineer"
+		9. Person's favourite colour: "Blue"
+		10. Current year: 2023
+		11. Number of followers on a social media platform: 1,000,000
+		12. Rating of a movie: 7.5
+		13. Person's blood type: 'A'
+		14. Title of a book: "To Kill a Mockingbird"
+		15. Number of employees in a company: 500
+		16. Time of an event: 2:30 PM
+		17. Name of a country: "United States"
+		18. Person's eye color: "Brown"
+		19. Person's birthplace: "New York City"
+		20. Distance between two cities: 200.5
+	 */
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Testing Assignments by Git Push.");
+		
+		//1. Temperature of a city in degrees Celsius: 25.5
+			float dCelsius = 25.5f;
+			System.out.println(dCelsius);
+		
+		//2. Whether a customer has placed an order: true or false
+			boolean orderCustomer = true;
+			System.out.println(orderCustomer);
+			
+		//3. Person's phone number: "123-456-7890"
+			int phoneNumber = 123456789;
+			System.out.println(phoneNumber);
+			
+		//4. Amount of money in a customer's bank account: 1000.50
+			float amountCustomer = 1000.50F;
+			System.out.println(amountCustomer);
+			
+		//5. Person's email address: "john.doe@example.com"
+			String email = "john.doe@example.com";
+			System.out.println(email);
+			
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+			double latitude = 37.7749;
+			System.out.println(latitude);
+			
+			double longitude = -122.4194;
+			System.out.println(longitude);
+			
+		//7. Person's marital status: true or false
+			boolean maritalStatus = false;
+			System.out.println(maritalStatus);
+			
+		//8. Person's occupation: "Software Engineer"
+			String occupation = "Software Engineer";
+			System.out.println(occupation);
+			
+		//9. Person's favourite colour: "Blue"
+			String favColor = "Blue";
+			System.out.println(favColor);
+			
+		//10. Current year: 2023
+			short currYear = 2023;
+			System.out.println(currYear);
+			
+		//11. Number of followers on a social media platform: 1,000,000
+			String followersSocial = "1,000,000";
+			System.out.println(followersSocial);
+			
+		//12. Rating of a movie: 7.5
+			float ratingMovie = 7.5f;
+			System.out.println(ratingMovie);
+			
+		//13. Person's blood type: 'A'
+			char bloodType = 'A';
+			System.out.println(bloodType);
+			
+		//14. Title of a book: "To Kill a Mockingbird"
+			String bookTitle = "To Kill a Mockingbird";
+			System.out.println(bookTitle);
+			
+		//15. Number of employees in a company: 500
+			short empCount = 500;
+			System.out.println(empCount);
+			
+		//16. Time of an event: 2:30 PM
+			String eventTime = "2:30 PM";
+			System.out.println(eventTime);
+			
+		//17. Name of a country: "United States"
+			String countryName = "Unites States";
+			System.out.println(countryName);
+			
+		//18. Person's eye color: "Brown"
+			String eyeColor = "brown";
+			System.out.println(eyeColor);
+			
+		//19. Person's birthplace: "New York City"
+			String birthPlace = "New York City";
+			System.out.println(birthPlace);
+			
+		//20. Distance between two cities: 200.5
+			float dCity = 200.5f;
+			System.out.println(dCity);
+		
 		
 	}
 
