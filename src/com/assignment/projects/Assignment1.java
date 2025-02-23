340c0f15092f0aef25e89bf0d2cb9e4dc83d8a47
shubhams673 <shubhams673@gmail.com>
2025-02-23 21:25:15 +0530

    first commit

---
+++ src/com/assignment/projects/Assignment1.java
@@ -0,0 +1,11 @@
+package com.assignment.projects;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testing Assignments by Git Push.");
+		
+	}
+
+}
